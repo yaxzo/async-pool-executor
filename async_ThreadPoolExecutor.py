48db01a57307f6e07705e085fce7d3af6c242eb4
yaxzo <100714739+yaxzo@users.noreply.github.com>
2023-02-18 13:42:11 +0300

    rename

--- async_ThreadPoolExecutor.py
+++ async_ThreadPoolExecutor.py
@@ -24,8 +24,8 @@ async def async_pool_executor(coros: list[typing.Awaitable[T]], workers=10) -> l
     queue: asyncio.Queue[tuple[int, typing.Awaitable[T]]] = asyncio.Queue()
     for i, coro in enumerate(coros):
         queue.put_nowait((i, coro))
-    niggers_work_result = await asyncio.gather(*[work(queue) for _ in range(workers)])
-    indexed_res: list[tuple[int, T]] = list(itertools.chain(*niggers_work_result))
+    work_result = await asyncio.gather(*[work(queue) for _ in range(workers)])
+    indexed_res: list[tuple[int, T]] = list(itertools.chain(*work_result))
     bar.close()
     res = [x[1] for x in sorted(indexed_res, key=lambda x: x[0])]
     logger.info(f"{time.time() - base_time}s spent on async_pool_executor *_*")
